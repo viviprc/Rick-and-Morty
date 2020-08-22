f444b8b211203ced39b904a6cfe5bcf732db8a85
Viviana <vivi_reyes_c@hotmail.com>
2020-08-22 19:24:24 -0300

    todo ok, sin paginador

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import rickAndMorty from './promesa'
 import $ from 'jquery'
 
-rickAndMorty.then((characters) => {
+
+
+const promesaCarga = rickAndMorty.then((characters) => new Promise((resolve) => {
   characters.forEach(e => {
     document.getElementById('cartoons').innerHTML += `<div class="cartoons-container"><img id="${e.id}" src="${e.imagen}">
       <ul>
-        <li>${e.nombre}</li>
+        <li><b>${e.nombre}</b></li>
         <li>${e.genero}</li>
         <li>${e.especie}</li>
         <li>${e.estado}</li>
       </ul>
     </div>`;
   });
-  $(document).ready(function(){
-    $('ul').hide();
-    
-     $('img').click(function(){
-       $(this).next('ul').toggle();
-    });
-    });
   
-  
-});
+  resolve()
+}));
 
+promesaCarga.then(() => {
+  $('ul').hide();
+    
+  $('img').click(function(){
+    $(this).next('ul').toggle();
+  });
+})
 
 
 
--- src/promesa.js
+++ src/promesa.js
@@ -1,22 +1,20 @@
 import axios from 'axios'
 import Character from './Personajes'
 
-const rickAndMorty = (() => {
-  return new Promise(async (resolve, reject) => {
-      try {
-          const llamadaApi = await axios.get("https://rickandmortyapi.com/api/character");
-          let information = llamadaApi.data.results
-          let characters = []
-          information.forEach(e => {
-              characters.push(new Character(e.name, e.id, e.gender,  e.species, e.status, e.image))
-          });
-          resolve(characters)
-      } catch (error) {
-          reject(`Tenemos un error (Promesa en rickAndMorty): ${error}`)
-      }
-
-      
-  })
+const rickAndMorty = (async() => {
+    try{
+        const llamadaApi= await axios.get("https://rickandmortyapi.com/api/character");
+        let information = llamadaApi.data.results
+        let characters = []
+        information.forEach(e => {
+            characters.push(new Character(e.name, e.id, e.gender,  e.species, e.status, e.image))
+        });
+        return characters
+    }
+    catch(error){
+        console.log(`Algo no esta bien, tienes el error:${error}`)
+    };
 })();
+
 
 export default rickAndMorty
